ed2b72b15cb6f0ab760b7b3b7016995b4545e93a
exlorvea <haryleblop@gmail.com>
2024-11-03 05:47:24 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,11 @@ window.onload = function() {
         document.getElementById('content').classList.remove('hidden');
     }, 4000); // 4000 ms = 4 seconds
 };
+function toggleSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section.classList.contains('hidden')) {
+        section.classList.remove('hidden');
+    } else {
+        section.classList.add('hidden');
+    }
+}
